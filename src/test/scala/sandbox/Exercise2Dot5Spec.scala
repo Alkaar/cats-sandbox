3078aeef3e30ee04b0384e52e4e8daaeb4f2bf50
Alkaar <Alkaar@gmail.com>
2019-09-18 00:43:28 -0400

    Removed comment

--- src/test/scala/sandbox/Exercise2Dot5Spec.scala
+++ src/test/scala/sandbox/Exercise2Dot5Spec.scala
@@ -18,20 +18,4 @@ class Exercise2Dot5Spec extends FlatSpec with Matchers {
   it should "combine Order elements" in {
     add(List(Order(2, 3), Order(4, 5))) shouldEqual Order(6, 8)
   }
-
-
-//  object Exercise2Dot5Spec {
-//    def add[A](items: List[A])(implicit monoid: Monoid[A]): A = items.foldLeft(Monoid[A].empty)(_ |+| _)
-//  }
-//
-//  object OrderMonoidInstance {
-//    implicit val orderMonoid: Monoid[Order] =
-//      new Monoid[Order] {
-//        def combine(a: Order, b: Order) = Order(a.totalCost |+| b.totalCost, a.quantity |+| b.quantity)
-//
-//        def empty = Order(0, 0)
-//      }
-//  }
-//
-//  case class Order(totalCost: Double, quantity: Double)
 }
